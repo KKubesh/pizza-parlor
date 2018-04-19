10e7f0ef85c91085968ae6472012e71d5f3f3926
Kerry Burns <kerryalanburns@gmail.com>
2018-04-19 10:37:22 -0500

    server stuff

--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
 
+//Route needed
+const pizzaRouter = require('./routes/pizza.router');
+
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/api/pizza', pizzaRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {

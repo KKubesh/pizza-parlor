ae4bdfea6de9614c680888adb2a9756096273493
Britni Marty <bjmarty23@gmail.com>
2018-04-19 11:44:47 -0500

    same thing

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,16 +1,38 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'; 
 
+
+const mapStateToProps = reduxState => ({
+    reduxState
+});
 
 class Menu extends Component{
+ componentDidMount() {
+     this.props.dispatch({
+         type: 'GET_MENU'
+     })
+ }
+
 
     render(){
-        return(
+        let menu = this.props.reduxState.menu.map((pizza) => {
+            return <p key= {pizza.id}>{pizza.name}{pizza.description}{pizza.cost}</p>
+        })
+        return (
+            <div>
             <h2> Menu</h2>
+            {menu}
+            </div>
         )
+
     }
-
-
 }
+// "pizza" ("name", "description", "cost")
+// VALUES ('Splat of Marinara', 'Cheeseless pizza with marinara, garlic and red peppers.', 12.99),
+// ('Onamonapizza', 'Cheese, BBQ sauce and artichokes.', 14.99),
+// ('Pepperoni', 'Classic pizza with cheese and pepperoni. Baked with a traditional crust in our brick oven.', 14.99),
+// ('Over the Rainbow', 'One ingredient of each color: pepperoni, doritos, pineapple, olives, cheese, peppers and onion.', 19.99),
+// ('Chinese Firedragon', 'Pepperoni, pineapple and banana peppers.', 15.99),
+// ('Bad Date', 'Garlic, Onion and Pepperoni.', 24.99
 
-
-export default Menu;
+export default connect(mapStateToProps)(Menu);

589e80b81a15a9672398f7acd0581e5efcc7d9c7
Kam Kubesh <kube0057@umn.edu>
2018-04-19 11:48:34 -0500

    checkout setup and checkItem.js added.

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,11 +1,50 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import CheckItem from '../CheckItem/CheckItem.js'
+
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
+
 
 
 class Checkout extends Component{
+    componentDidMount() {
+        // use component did mount to dispatch an action to request the checkoutList from the API
+        this.props.dispatch({
+            type: 'CHECKOUT_LIST'
+        })
+    }
+
+    handleClick= () => {
+        this.props.dispatch({ 
+            type: 'ADD_ORDER', 
+            payload: this.props.checkoutList
+        })
+    }
 
     render(){
+        let itemList = this.props.reduxState.checkoutList.map(item => {
+            return (
+                <CheckItem key={item.id} item={item}/>
+            )
+        })
+        
         return(
-            <h2>Checkout</h2>
+            <div>
+                <h2>Checkout</h2>
+                <input type="text" placeholder="Your Name" />
+                <table>
+                    <thead>
+                        <tr><th>Name</th><th>Quantity</th><th>Cost</th></tr>
+                    </thead>
+                    <tbody>
+                        {itemList}
+                    </tbody>
+                </table>
+                {this.props.reduxState.checkoutList.totalCost}
+                <button onClick={this.handleClick}>Check Out</button>
+            </div>
         )
     }
 
@@ -13,4 +52,4 @@ class Checkout extends Component{
 }
 
 
-export default Checkout;
+export default connect(mapStateToProps)(Checkout);

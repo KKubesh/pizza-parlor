bacd8f621fc37aff3ab03dace3c75627190842af
Britni Marty <bjmarty23@gmail.com>
2018-04-20 10:43:01 -0500

    pizza component added

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,58 +1,43 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux'; 
+import React, {
+    Component
+} from 'react';
+import {
+    connect
+} from 'react-redux';
+import Pizza from './Pizza/Pizza';
 
 
 const mapStateToProps = reduxState => ({
     reduxState
 });
 
-class Menu extends Component{
-    state={
-        newOrder: {
-            //need this being sent to server individually
-            id: '',
-            name: '',
-            description: '',
-            cost: ''
-        }
-    }
-    
-// server request for menu item   
- componentDidMount() {
-     this.props.dispatch({
-         type: 'GET_MENU'
-     })
- }
- //post request for order details
- addNewOrder = (order) => {
-    //  console.log(this.state.newOrder)
-     this.props.dispatch({
-         type: 'NEW_ORDER',
-         payload: this.state.newOrder
-     })
- }
-//  handleClick = () => {
-//      console.log('in delete button');
-
-//  }
-
-
-
-
-    render(){
+class Menu extends Component {
+    render() {
+        console.log(this.props.reduxState)
         let menu = this.props.reduxState.menuReducer.map((pizza) => {
-            return <p key= {pizza.id}>{pizza.name}{pizza.description}{pizza.cost}
-            <button Onclick="this.handleClick">-</button><button Onclick="this.handleClickAdd">+</button></p>
+            return ( 
+                <Pizza key = {
+                    pizza.id
+                }
+                name = {
+                    pizza.name
+                }
+                description = {
+                    pizza.description
+                }
+                cost = {
+                    pizza.cost
+                }
+                pizza = {
+                    pizza
+                }
+                />
+            )
         });
-        return (
-            <div>
-            <h2> Menu</h2>
-            {menu}
-            <button onClick={this.addNewOrder}>Add order</button>
-            {/* <pre>{JSON.stringify(this.state)}</pre>  */}
-            </div>
-        )
 
+        return ( 
+            menu
+        )
     }
 }
 
